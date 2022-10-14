0615158dabdc7d90ab002c64b6be82e56bce6535
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-10-14 19:36:56 +0530

    Update and rename Q18 to Q18.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q18.c
@@ -0,0 +1,16 @@
+//Q18) PROGRAM TO PRINT INCREMENT OPERATOR FOR 3 DIGIT NUMBERS
+
+//INPUT: 
+
+#include <stdio.h>
+
+int main() {
+  int x = 369;
+  printf("%d", ++x);
+  return 0;
+}
+
+
+//OUTPUT:
+
+//340
