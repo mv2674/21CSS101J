7411438ceb58396c42f93e4099dda077823c953c
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-10-23 15:22:18 +0530

    Create Q30.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q30.c
@@ -0,0 +1,19 @@
+//Q30) WRITE A PROGRAM USING FLOATING VALUES.
+
+//INPUT
+
+#include<stdio.h>
+#include<string.h>
+int main() {
+   float x = 10.327;
+   int y = 28;
+   printf("The float value : %f", x);
+   printf("The sum of float and int variable : %f", (x+y));
+   return 0;
+}
+
+
+//OUTPUT
+
+//The float value : 10.327000
+//The sum of float and int variable : 38.327000
