b6e5d04daf1647faa1f6ccc9001e9b4b6fa04e96
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-10-23 15:28:02 +0530

    Create Q31.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q31.c
@@ -0,0 +1,19 @@
+//Q31) WRITE A PROGRAM USING FLOAT 
+
+//INPUT
+
+#include <stdio.h>
+
+int main()
+{
+   float age;
+
+   age = 35.5;
+   printf("SRM is over %f years old.\n", age);
+
+   return 0;
+}
+
+//OUTPUT
+
+//SRM is over 35.500000 years old
