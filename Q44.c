a5a25a3def93892c13e5528f2e78322bca5ed377
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-10-31 20:39:31 +0530

    Create Q44.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q44.c
@@ -0,0 +1,14 @@
+//Q44) PROGRAM A BITWISE OR OPERATOR
+
+//INPUT
+
+#include <stdio.h>
+int main() {
+    int a = 100, b = 250;
+    printf("Output = %d", a | b);
+    return 0;
+}
+
+//OUTPUT
+
+//Output = 254
