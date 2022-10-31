9c257583f863675bc673a9eb4a96d80b82d5b1ab
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-10-31 20:35:14 +0530

    Create Q42.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q42.c
@@ -0,0 +1,14 @@
+//Q42) PROGRAM A BITWISE AND OPERATORS
+
+//INPUT
+
+#include <stdio.h>
+int main() {
+    int a = 100, b = 500, c=250;
+    printf("Output = %d", a & b & c);
+    return 0;
+}
+
+//OUTPUT
+
+//Output = 96
