77e7672901cb15cfb0ae0bb335b65ceb06987a53
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-10-31 19:17:39 +0530

    Create Q37.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q37.c
@@ -0,0 +1,14 @@
+//Q37) PROGRAM A BITWISE AND OPERATOR
+
+//INPUT
+
+#include <stdio.h>
+int main() {
+    int a = 42, b = 69;
+    printf("Output = %d", a & b);
+    return 0;
+}
+
+//OUTPUT
+
+//Output = 0
