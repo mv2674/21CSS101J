d794a6a5a82fb45d7afbc530850fd8044660224d
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-10-31 19:10:28 +0530

    Create Q34.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q34.c
@@ -0,0 +1,14 @@
+//Q34) PROGRAM TO PRINT BITWISE AND OPERATOR
+
+//INPUT
+
+#include <stdio.h>
+int main() {
+    int a = 12, b = 25;
+    printf("Output = %d", a & b);
+    return 0;
+}
+
+//OUTPUT
+
+//Output = 8
