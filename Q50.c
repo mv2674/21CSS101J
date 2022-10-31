d14740293cd7d7dcc0965c0151698f25b197e5c6
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-10-31 21:05:44 +0530

    Create Q50.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q50.c
@@ -0,0 +1,15 @@
+//Q50) PROGRAM A BITWISE COMPLIMENT
+
+//INPUT
+
+#include <stdio.h>
+int main() {
+    printf("Output = %d\n", ~-420);
+    printf("Output = %d\n", ~-100);
+    return 0;
+}
+
+//OUTPUT
+
+//Output = 419
+//Output = 99
