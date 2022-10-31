a5bf5156ccba2561109d7cc385a5feeca5838d9e
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-10-31 19:12:14 +0530

    Create Q35.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q35.c
@@ -0,0 +1,14 @@
+//Q35) PROGRAM A BITWISE OR OPERATOR
+
+//INPUT
+
+#include <stdio.h>
+int main() {
+    int a = 12, b = 25;
+    printf("Output = %d", a | b);
+    return 0;
+}
+
+//OUTPUT
+
+//Output = 29
