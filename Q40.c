00dca48671dde5c017cb260efe8740e29c08f97e
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-10-31 19:36:29 +0530

    Create Q40.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q40.c
@@ -0,0 +1,14 @@
+//Q40) PROGRAM A BITWISE AND OPERATOR
+
+//INPUT
+
+#include <stdio.h>
+int main() {
+    int a = 50, b = 50;
+    printf("Output = %d", a & b);
+    return 0;
+}
+
+//OUTPUT
+
+//Output = 50
