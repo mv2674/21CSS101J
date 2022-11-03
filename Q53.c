610222966a3fe071700a8cd17fb10917704c1057
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-03 18:22:36 +0530

    Create Q53.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q53.c
@@ -0,0 +1,14 @@
+//PROGRAM ANY EXAMPLE OF INCREMENT FUNCTIONS
+
+//INPUT
+
+#include <stdio.h>
+int main() {
+  int x = 30;
+  printf(" %d, %d, %d ", ++x, x, x++);
+  return 0;
+}
+
+//OUTPUT
+
+//32, 32, 30
