53b2bdbef5459626c685a903a1fb2e424f6e99ea
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-03 18:16:54 +0530

    Create Q52.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q52.c
@@ -0,0 +1,17 @@
+//Q52) PROGRAM TO SHOW PRE INCREMENT AND POST INCREMENT OF X,Y VALUES
+
+//INPUT
+
+#include <stdio.h>
+int main() {
+  int x=10, y=15, a,b;
+  a= ++x;
+  b= y++;
+  printf("%d, %d", a, b);
+  return0;
+}
+
+//OUTPUT
+
+//11, 15
+  
