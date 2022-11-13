c755bfb3d837d6b16b664e227f682d8b70b27c78
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-13 20:21:34 +0530

    Create Q56.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q56.c
@@ -0,0 +1,15 @@
+//PROGRAM TO UNDERSTAND TYPECASTE EXPERIMENTS
+
+//INPUT
+
+#include <stdio.h>
+void main () {
+	float x, y;
+	x = 7 / 5;
+	y = (float) 7 / 5;
+	printf("x = %f, y = %f\n", x, y);
+}
+
+//OUTPUT
+
+//x = 1.000000, y = 1.400000
