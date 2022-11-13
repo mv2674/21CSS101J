67651bf95d3727e13eab59fa2a42d26f4dc0295f
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-13 20:16:34 +0530

    Create Q55.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q55.c
@@ -0,0 +1,13 @@
+//Q55) PROGRAM TO PERFORM BITWISE OPERATIONS
+
+//INPUT
+
+#include <stdio.h>
+void main() {
+  int num1 = 10 , num2 = 3, result;
+  result = num1&num2;
+  printf("Bitwise AND result = %d\n", result);
+}
+  
+//OUTPUT
+//Bitwise AND result = 2
