8b295cce15a3a7e8ff238747b5e83871ea81f061
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-20 14:32:48 +0530

    Create Q60.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q60.c
@@ -0,0 +1,17 @@
+//Write a program to read two integers from standard input and to print the sum of the two given integers on the standard output.
+
+//Input
+
+#include <stdio.h>
+int main() {
+int a, b, sum;
+  printf("enter two integers: ");
+  scanf("%d %d", &a, &b);
+  sum = a+b;
+  printf("sum = %d\n", sum);
+
+}
+
+//Output
+//enter·two·integers : 3 4
+//sum = 7
