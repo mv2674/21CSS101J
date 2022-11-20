9f976ee7f19e9e665efb73285ebde547e68bc5fd
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-20 22:22:12 +0530

    Create Q68.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q68.c
@@ -0,0 +1,17 @@
+//Program a simple if c program
+
+//input
+
+#include <stdio.h>
+int main() {
+	int a = 500, b = 100, c;
+	if (!a >= 400)
+		b = 300;
+	c = 200;
+	printf("b = %d c = %d\n", b, c);
+	return 0;
+}
+
+//output
+
+//b = 100 c = 200
