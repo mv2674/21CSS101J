5a57ede6bba80258cde6e7ac8bd10aa34c855b93
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-21 00:47:00 +0530

    Create Q71.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q71.c
@@ -0,0 +1,17 @@
+//Program to print hi or hello
+
+//input
+
+#include <stdio.h>
+void main() {
+	float a = 0.7;
+	if (0.7 > a) {
+		printf("Hi\n");
+	} else {
+		printf("Hello\n");
+	}
+}
+
+//output
+
+//Hi
