42f9cb7f658171a1916921f70ff1b0d892527421
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-20 14:37:55 +0530

    Create Q61.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q61.c
@@ -0,0 +1,31 @@
+//PROGRAM ALL THE ARTHIMETIC OPERATORS
+
+//INPUT
+
+#include <stdio.h>
+
+void main() {
+
+int a, b, s1, s2, s3, s4, s5;
+  printf("Enter any two integer values: ");
+  scanf("%d %d", &a, &b);
+  s1 = a+b; 
+  s2 = a-b; 
+  s3 = a*b ; 
+  s4= a/b ; 
+  $5 = ab; 
+  printf("Sum = %d\n", s1); 
+  printf("Difference = %d\n",s2 ); 
+  printf("Product = %d\n",s3); 
+  printf("Division = %d\n",s4 ); 
+  printf("Remainder = %d\n", s5); 
+}
+
+//OUTPUT
+
+//Enter·any·two·integer·values·:·10 5
+//Sum·=·15
+//Difference·=·5
+//Product·=·50
+//Division·=·2
+//Remainder·=·0
