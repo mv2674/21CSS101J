9dc5a304e6bcc52db38a580c59841d5ad8ecbf75
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-20 22:17:07 +0530

    Create Q67.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q67.c
@@ -0,0 +1,17 @@
+//Program using if program to say a and b are equal
+
+//Input
+
+#include <stdio.h>
+void main() {
+    int a = 3;
+    float b = 3.0;
+    if (a == b)
+        printf("a and b are equal");
+    if (a != b)
+        printf("a and b are not equal"); 
+}
+
+//Output
+
+//a and b are equal
