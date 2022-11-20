cbb2d60e772d43b2f3724d198e23168eb41d4bcd
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-20 14:43:40 +0530

    Create Q62.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q62.c
@@ -0,0 +1,18 @@
+//Program to find the sum of an int and a float value.
+
+//INPUT
+
+#include <stdio.h>
+void main() { 
+  int intValue;
+  float floatValue, sum;
+  printf("Enter an int and a float value: ");
+  scanf("%d %f", &intValue, &floatValue);
+  sum intValue+floatValue; 
+  printf("Sum of %d and %f = %f\n", intValue, floatValue, sum);
+}
+
+//output
+
+//Enter·an·int·and·a·float·value·:·3 5.6
+//Sum·of·3·and·5.600000·=·8.600000
