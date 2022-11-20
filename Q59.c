33041c115acdff4042cd18dcaeb2970cbb28cf43
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-20 14:17:24 +0530

    Create Q59.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q59.c
@@ -0,0 +1,17 @@
+//PROGRAM USING GETCHAR AND PUTCHAR
+
+//INPUT
+
+#include <stdio.h>
+void main() {
+  char ch;
+  printf("Enter a charecter : ");
+  ch = getchar();
+  printf("The entered charecter is : ");
+  putchar(ch);
+}
+
+
+//OUTPUT
+//Enter a charecter : E
+//The entered charecter is : E
