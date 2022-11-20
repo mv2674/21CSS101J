c46f7cca2cbd756cef473fb7ab949c8f6752dfba
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-20 22:14:44 +0530

    Create Q66.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q66.c
@@ -0,0 +1,16 @@
+//Program a simple if program
+
+//Input
+
+#include <stdio.h>
+void main() {
+	int a = 20, b = 30, c = 40;
+	if (a > 30)
+		b = 25;
+		c = 35;
+	printf("%d %d", b, c);
+}
+
+//Output
+
+//30 35
