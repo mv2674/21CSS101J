23f6992f9a304967105371e1a4a7938f154451c2
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-21 22:55:21 +0530

    Create Q73.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q73.c
@@ -0,0 +1,21 @@
+//program a basic C program to show switch funclion
+
+//input
+
+#include <stdio.h>
+void main() {
+	int i = 1;
+	switch (i) {
+		printf("This is c program");
+		case 1:
+			printf("Case-1");
+			break;
+		case 2:
+			printf("Case-2");
+			break;
+	}
+}
+
+//output
+
+//Case-1
