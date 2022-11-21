d294a93566a72a7a43905d339c3cfbaebc92709b
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-21 22:58:09 +0530

    Create Q74.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q74.c
@@ -0,0 +1,20 @@
+//Program a simple switch program  
+
+//Input
+
+#include <stdio.h>
+void main() {
+	int choice = -2 ;
+	switch (choice) {
+		case -1:
+			printf("Case: -1\n");
+			break;
+		case -2:
+			printf("Case: -2\n");
+			break;
+	}
+}
+
+//Output
+
+//Case: -2
