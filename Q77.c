c2de7e816c239f98ef4d857710bdc90f229dd94d
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-22 13:34:57 +0530

    Create Q77.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q77.c
@@ -0,0 +1,16 @@
+//Program a while loop program
+
+//input
+
+#include <stdio.h>
+void main() {
+  int i = 3;
+  while (i--) {
+    int i = 100;
+    i--;
+    print("%d", i);
+  }
+}
+
+//OUTPUT
+//99 99 99
