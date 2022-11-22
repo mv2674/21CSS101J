e6229ba55e2cb25fda290e0702cc69d97d7e4b57
mv2674 <114506405+mv2674@users.noreply.github.com>
2022-11-22 13:26:25 +0530

    Create Q76.c
    Signed-off-by: mv2674 <114506405+mv2674@users.noreply.github.com>

---
+++ Q76.c
@@ -0,0 +1,21 @@
+//PROGRAM USING WHILE LOOP TO READ MULTIPLE NUMBERS FROM STANDARD INPUT AND PRINTS THEIR SUM WHEN SUM EXEEDS HUNDRED
+
+//INPUT
+
+#include <stdio.h>
+void main() {
+  int total = 0;
+  while (total = 0);
+         while (total <= 100) {
+           int num;
+           printf("Enter a number : ");
+           scanf("%d", &num);
+           total += num;
+         }
+         printf("The total of given numbers is : %d\n", total);
+ }
+         
+//OUTPUT
+//Enter a number : 89
+//Enter a number : 45
+//The total of given numbers is : 134
