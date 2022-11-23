1204205d10b734e77ed82101eb733849f2526329
Medha Vasireddi <114506405+mv2674@users.noreply.github.com>
2022-11-23 19:09:25 +0530

    Create Q82.c
    Signed-off-by: Medha Vasireddi <114506405+mv2674@users.noreply.github.com>

---
+++ Q82.c
@@ -0,0 +1,31 @@
+//C program to check whether a character is alphabet, digit or special character
+
+//Input
+
+include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+    if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    {
+        printf("'%c' is alphabet.", ch);
+    }
+    else if(ch >= '0' && ch <= '9')
+    {
+        printf("'%c' is digit.", ch);
+    }
+    else 
+    {
+        printf("'%c' is special character.", ch);
+    }
+
+    return 0;
+}
+
+//output
+
+//Enter any character: 5
+//'5' is digit.
+  
