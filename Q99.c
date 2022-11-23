577be17919d7ca17733d42f7e1b518eba32ba93d
Medha Vasireddi <114506405+mv2674@users.noreply.github.com>
2022-11-23 22:41:08 +0530

    Create Q99.c
    Signed-off-by: Medha Vasireddi <114506405+mv2674@users.noreply.github.com>

---
+++ Q99.c
@@ -0,0 +1,20 @@
+//C program to print natural numbers using while loop 
+
+//Input
+
+#include <stdio.h>
+int main() 
+{  
+    int n = 1;  
+    while(n <= 10) {
+        printf("%d ", n); 
+        n++; 
+    }
+    return 0;
+}
+
+//Output
+
+//1 2 3 4 5 6 7 8 9 10 
+
+
