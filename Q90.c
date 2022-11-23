3356b7c83d70ff40fa35c906fdbb99d98442c891
Medha Vasireddi <114506405+mv2674@users.noreply.github.com>
2022-11-23 21:09:53 +0530

    Create Q90.c
    Signed-off-by: Medha Vasireddi <114506405+mv2674@users.noreply.github.com>

---
+++ Q90.c
@@ -0,0 +1,27 @@
+//C program to find maximum between two numbers using switch case
+
+//Input
+
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    printf("Enter two numbers to find maximum: ");
+    scanf("%d%d", &num1, &num2);
+    switch(num1 > num2)
+    {  
+        case 0: 
+            printf("%d is maximum", num2);
+            break;
+        case 1: 
+            printf("%d is maximum", num1);
+            break;
+    }
+    return 0;
+}
+
+//Output
+
+//Enter two numbers to find maximum: 69 420
+//420 is maximum
+
