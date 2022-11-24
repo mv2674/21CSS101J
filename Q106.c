dd2b397552b43595f6dd372eb8d21f8a642a73c4
Medha Vasireddi <114506405+mv2674@users.noreply.github.com>
2022-11-24 21:51:29 +0530

    Create Q106.c
    Signed-off-by: Medha Vasireddi <114506405+mv2674@users.noreply.github.com>

---
+++ Q106.c
@@ -0,0 +1,26 @@
+//Program to update contents of arrays
+
+//Input
+
+#include<stdio.h>
+void main(void)
+{
+  float number;
+  printf("decimal values betwwen 1.0 and 10.0are :-\n");
+  for (number = 2.0; number < 10.0; number ++)
+  {printf("%f\n",number);}
+}
+
+//Output
+
+/*
+decimal values betwwen 1.0 and 10.0are :-
+2.000000
+3.000000
+4.000000
+5.000000
+6.000000
+7.000000
+8.000000
+9.000000
+*/
